485b5315dd68a1142436d7473195dbc9972e1aeb
whosethink <whosethink@gmail.com>
2021-07-26 20:13:17 +0800

    feature: use modular of 26 as shift

--- src/args.rs
+++ src/args.rs
@@ -1,23 +1,26 @@
 use clap::ArgMatches;
+use std::ops::Rem;
+
+const SHIFT_MOD: i8 = 26;
 
 #[derive(Debug)]
 pub struct Args {
   shift: i8,
-  text: String
+  text: String,
 }
 
 impl Args {
-
   pub fn from_matches(matches: &ArgMatches) -> Args {
     let shift = matches.value_of("shift")
-      .unwrap();
-    let shift = shift.parse::<i8>()
-      .unwrap();
+      .unwrap()
+      .parse::<i8>()
+      .unwrap()
+      .rem(SHIFT_MOD);
     let text = matches.value_of("text")
       .unwrap().to_string();
     Args {
       shift,
-      text
+      text,
     }
   }
 
